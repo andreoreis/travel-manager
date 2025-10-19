51ce3c9515d1ad4ce669d9f27b6f2c7b24504aea
areis <andre_reis2011@hotmail.com>
2025-10-19 02:33:13 -0300

    feat: Criando BaseResource

---
+++ backend/app/Http/Resources/BaseResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BaseResource extends JsonResource
+{
+    /**
+     * Tranforma o recurso em um array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}
