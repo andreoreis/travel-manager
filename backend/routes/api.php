b520ed6ac08d1c4bf9133a6ee472078950f0dbb1
areis <andre_reis2011@hotmail.com>
2025-10-19 02:38:07 -0300

    feat: Definindo rotas travel-requests

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,13 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelRequestController;
 
+//Auth routes
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login',    [AuthController::class, 'login']);
 
 // Rotas protegidas por auth
 Route::middleware('auth:api')->group(function () {
+    //Auth routes
     Route::post('auth/logout',  [AuthController::class, 'logout']);
     Route::post('auth/refresh', [AuthController::class, 'refresh']);
     Route::get('auth/me',       [AuthController::class, 'me']);
+
+    //TravelRequests routes
+    Route::apiResource('travel-requests', TravelRequestController::class);
+    Route::post('travel-requests/{travel_request}/status', [TravelRequestController::class, 'changeStatus']);
 });
