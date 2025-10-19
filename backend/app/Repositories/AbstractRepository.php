446b949e707adbc24d21923a92787f25f29554cf
areis <andre_reis2011@hotmail.com>
2025-10-19 00:47:46 -0300

    feat: Criando AbstractRepository

---
+++ backend/app/Repositories/AbstractRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Interfaces\RepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository implements RepositoryInterface
+{
+    protected static $model;
+
+    public static function all():Collection{
+        return self::loadModel()::all();
+    }
+
+    public static function find(int $id):Model|null{
+        return self::loadModel()::query()->find($id);
+    }
+
+    public static function create(array $data = []):Model|null{
+        return self::loadModel()::query()->create($data);
+    }
+
+    public static function delete(int $id):bool{
+        return self::loadModel()::query()->where(['id' => $id])->delete();
+    }
+
+    public static function update(int $id, array $data = []):int{
+        return self::loadModel()::query()->where(['id' => $id])->update($data);
+    }
+
+    public static function loadModel():Model{
+        return app(static::$model);
+    }
+}
