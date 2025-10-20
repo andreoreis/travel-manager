f89ccc2ea221965c15adc4afe87a3cf87c038367
areis <andre_reis2011@hotmail.com>
2025-10-20 04:02:09 -0300

    fix: Corrigindo instância de middleware admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TravelRequestController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login',    [AuthController::class, 'login']);
@@ -13,9 +14,7 @@ Route::middleware('auth:api')->group(function () {
     Route::get('auth/me',       [AuthController::class, 'me']);
 
     Route::apiResource('travel-requests', TravelRequestController::class);
-});
 
-Route::middleware(['auth:api', 'admin'])->group(function () {
-    Route::patch('travel-requests/{travel_request}/status', [TravelRequestController::class, 'changeStatus']);
-    Route::patch('travel-requests/{travel_request}/cancel', [TravelRequestController::class, 'cancel']);
+    Route::patch('travel-requests/{travel_request}/status', [TravelRequestController::class, 'changeStatus'])->middleware([AdminMiddleware::class]);
+    Route::patch('travel-requests/{travel_request}/cancel', [TravelRequestController::class, 'cancel'])->middleware([AdminMiddleware::class]);
 });
